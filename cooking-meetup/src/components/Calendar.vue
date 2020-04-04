d7dfdb9eeec5ceb35613257193a605c509565336
Johannes Teklote <johannes.teklote@web.de>
2020-04-05 01:46:04 +0200

    add calendar data

--- cooking-meetup/src/components/Calendar.vue
+++ cooking-meetup/src/components/Calendar.vue
@@ -12,29 +12,15 @@
 </template>
 
 <script>
-  import { DaykeepCalendarMonth } from '@daykeep/calendar-quasar'
+  import { DaykeepCalendarMonth } from '@daykeep/calendar-quasar';
+  import eventdata from '../data/eventdata';
 
   export default {
     name: "Calendar",
     components: { DaykeepCalendarMonth },
     data() {
       return {
-        eventData: [
-          {
-            id: 1,
-            summary: 'VersusVirus Launch Dinner',
-            description: 'Dinner Cooking after launch of VersusVirus Hackathon.',
-            start: {
-              dateTime: '2020-04-03T19:00:00', // ISO 8601 formatted
-              timeZone: 'Europe/Berlin' // Timezone listed as a separate IANA code
-            },
-            end: {
-              dateTime: '2020-04-03T21:00:00',
-              timeZone: 'Europe/Berlin'
-            },
-            color: 'positive',
-          }
-        ]
+        eventData: eventdata
       }
     }
   }
