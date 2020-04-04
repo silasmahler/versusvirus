5f029bd11fd250257fc3b1a24c3ec65587a4799e
Silas Mahler <silas-mahler@t-online.de>
2020-04-04 15:54:11 +0200

    add

--- cooking-meetup/src/i18n/en-us/index.js
+++ cooking-meetup/src/i18n/en-us/index.js
@@ -4,6 +4,7 @@
 export default {
   failed: 'Action failed',
   success: 'Action was successful',
+
   stepper: {
     selectPeopleGroup: {
       title: 'Select cook mates',
@@ -16,5 +17,12 @@ export default {
     cookWanted: {
       
     }
-  }
+  },
+ topics: {
+  leaf_to_root: 'Leaf to Root',
+  leaf_to_root_desc: 'Vegetables from leaf to root, fruit from skin to core: "Leaf to Root" is the nose to tail for plants. More and more culinary delicacies are being made from otherwise little-appreciated vegetable parts such as pea peel or fennel stalks.',
+
+  urban_agriculture: 'Urban Agriculture',
+  urban_agriculture_desc: 'The city is a conurbation and focal point of consumption. However, many foodstuffs are still produced in the countryside. But there are other ways. Urban Agriculture is committed to a diverse flora and fauna, also and especially in urban settlement areas.'
+ }
 }
