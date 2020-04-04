2948331910a5595499e155e932bda543bf28f27b
Christoph Weinzierl <granner@arcor.de>
2020-04-04 20:58:56 +0200

    Add index link to toolbar title

--- cooking-meetup/src/layouts/MainLayout.vue
+++ cooking-meetup/src/layouts/MainLayout.vue
@@ -11,30 +11,16 @@
           @click="leftDrawerOpen = !leftDrawerOpen"
         />
 
-        <q-toolbar-title>
-          Cooking Meetup
-        </q-toolbar-title>
+        <router-link to="/" class="toolbar-title-link">
+          <q-toolbar-title>Cooking Meetup</q-toolbar-title>
+        </router-link>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header class="text-grey-8">Essential Links</q-item-label>
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -45,34 +31,32 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
+import EssentialLink from "components/EssentialLink";
 
 export default {
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     EssentialLink
   },
 
-  data () {
+  data() {
     return {
-      languages: [
-        'english', 'french', 'german', 'italian'
-      ],
+      languages: ["english", "french", "german", "italian"],
       leftDrawerOpen: false,
       essentialLinks: [
         {
-          title: 'Profile',
-          icon: 'school',
-          link: '#/profile'
+          title: "Profile",
+          icon: "school",
+          link: "#/profile"
         },
         {
-          title: 'Colors',
-          icon: 'school',
-          link: '#/colors'
+          title: "Colors",
+          icon: "school",
+          link: "#/colors"
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
