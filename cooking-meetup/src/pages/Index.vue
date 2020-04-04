7067441d82a2c88a5b981850314655c754f8b2de
Silas Mahler <silas-mahler@t-online.de>
2020-04-04 14:45:54 +0200

    add some buttons

--- cooking-meetup/src/pages/Index.vue
+++ cooking-meetup/src/pages/Index.vue
@@ -1,14 +1,22 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn color="white" text-color="black" label="Standard" />
+      <q-btn color="primary" label="Primary" />
+      <q-btn color="secondary" label="Secondary" />
+      <q-btn color="accent" label="accent" />
+      <q-btn color="dark" label="dark" />
+      <q-btn color="positive" label="positive" />
+      <q-btn color="negative" label="negative" />
+      <q-btn color="info" label="info" />
+      <q-btn color="warning" label="warning" />
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "PageIndex"
+};
 </script>
