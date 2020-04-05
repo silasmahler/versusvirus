2d25ec32b6a53ef85bd84cd375d63d97ef1785b5
Silas Mahler <silas-mahler@t-online.de>
2020-04-05 15:14:49 +0200

    add italian

--- cooking-meetup/src/i18n/it/index.js
+++ cooking-meetup/src/i18n/it/index.js
@@ -1,108 +1,85 @@
 export default {
-  nav: {
-    title: 's'
-  },
-
+  nav: { title: "s" },
   stepper: {
     selectPeopleGroup: {
-      title: 'Koch-Partner auswählen',
-      text: 'Please select whether you want to cook with friends or strangers:',
+      title: "Seleziona le persone per fare la cucina",
+      text: "Scegli si vuoi cucinare con amici o con persone sconosciute:",
     },
-    people: {
-      friends: 'Friends',
-      strangers: 'Strangers',
-    },
+    people: { friends: "Amici", strangers: "Persone sconosciute" },
     chefWanted: {
-      title: 'Chef support wanted?',
-      text: 'Please choose if you want to be supported by a professional chef:'
+      title: "Vuoi l’aiuta d’un chef?",
+      text:
+        "Per favore sceglie se vuole essere aiutato da uno chef professionale:",
     },
     topics: {
-      title: 'Choose topics',
-      text: 'Select a topic of your choice',
-
-      start_session: 'Start session!',
-
-      categories: {
-        vegetarian: 'Vegetarian',
-        vegan: 'Vegan'
-      },
-
-      leaf_to_root: 'Leaf to Root',
-      leaf_to_root_desc: 'Vegetables from leaf to root, fruit from skin to core: "Leaf to Root" is the nose to tail for plants. More and more culinary delicacies are being made from otherwise little-appreciated vegetable parts such as pea peel or fennel stalks.',
-
-      urban_agriculture: 'Urban Agriculture',
-      urban_agriculture_desc: 'The city is a conurbation and focal point of consumption. However, many foodstuffs are still produced in the countryside. But there are other ways. Urban Agriculture is committed to a diverse flora and fauna, also and especially in urban settlement areas.'
+      title: "Scegliere tema",
+      text: "Scegli il tema che ti piace",
+      start_session: "Inizio sessione!",
+      categories: { vegetarian: "Vegetariano", vegan: "Vegano" },
+      leaf_to_root: "Dalla foglia a le radici",
+      leaf_to_root_desc:
+        "Verdure dalla foglia a le radici, della frutta alla buccia al nucleo: “Dalla foglia a le radici” e il dal naso alle dita dei piedi per le piante. Sempre più piatti di grande cucina sono fatti con parte di verdura poco gradite come la buccia di pisello o i gambi di finocchio.",
+      urban_agriculture: "Agricultura urbana",
+      urban_agriculture_desc:
+        "La città è la conurbazione e il punto focale della consumazione. Pero, la maggior parte del cibo è prodotta in campagnia. Esistono altre possibilità. L’agricultura urbana s’impegna per una flora e una fauna con diversità, anche e specialmente nei luoghi d’abitazione urbani.",
     },
   },
   buttons: {
-    continue: 'Continue',
-    back: 'Back',
-    save: 'Save',
-    login: 'Login',
-    logout: 'Logout',
+    continue: "Continua",
+    back: "Indietro",
+    save: "Memorizzare",
+    login: "Accesso",
+    logout: "Disconnetersi",
+    leave: "Paritre",
+    cancel: "Cancelare",
   },
   tabs: {
-    join: {
-      title: 'Join a room',
-      tabTitle: 'Join room'
-    },
-    create: {
-      title: 'Create a room',
-      tabTitle: 'Create room'
-    }
+    join: { title: "Entra in una stanza", tabTitle: "Unisciti alla stanza" },
+    create: { title: "Creare stanza", tabTitle: "Crea una stanza" },
+    calendar: { title: "Imminente", tabTitle: "Imminente" },
   },
   createRoom: {
-    topicName: 'Topic name',
-    categorySelect: 'Select a category',
-    description: 'Description',
+    topicName: "Nome del tema",
+    categorySelect: "Seleziona una categoria",
+    description: "Descrizione",
     categories: {
-      vegan: 'vegan',
-      traditional: 'traditional',
-      french: 'french',
-      german: 'german',
-      italian: 'italian'
+      vegan: "vegano",
+      traditional: "tradizionale",
+      french: "francese",
+      german: "tedesco",
+      italian: "italiano",
     },
-    createButton: 'Create New Room',
-    openButton: 'Open Room',
-    copyButton: 'Copy Room URL'
+    createButton: "Creare stanza",
+    openButton: "Aprire stanza",
+    copyButton: "Copiare  l’URL della stanza",
   },
   login: {
-    username: 'Username',
-    password: 'Password',
-    error: 'Either username or password are wrong'
+    username: "Nome utente",
+    password: "Password",
+    error: "Il nome d’utente o il password sono incorretti",
   },
   profile: {
-    name: 'Name',
-    vitae: 'Vitae',
-    niche: 'Niche',
+    name: "Nome",
+    vitae: "Curriculum Vitae",
+    niche: "Niche",
     devices: {
-      title: 'Devices',
-      smartphone: {
-        label: 'Smartphone',
-        hint: 'Hint for Smartphones'
-      },
-      notebook: {
-        label: 'Notebook',
-        hint: 'Hint for Notebooks'
-      },
-      camera: {
-        label: 'Camera',
-        hint: 'Hint for Cameras'
-      }
-    }
+      title: "Devices",
+      smartphone: { label: "Smartphone", hint: "Hint for Smartphones" },
+      notebook: { label: "Notebook", hint: "Hint for Notebooks" },
+      camera: { label: "Camera", hint: "Hint for Cameras" },
+    },
   },
-  menu: {
-    profile: 'Profile'
+  room: {
+    leaveRoomMessage: "You are about to leave the current cooking session.",
+    currentRecipe: "Current Recipe",
   },
+  menu: { profile: "Profile" },
   languages: {
-    english: 'English',
-    french: 'French',
-    german: 'German',
-    italian: 'Italian',
-    chooseLanguage: 'Choose Language'
+    english: "English",
+    french: "French",
+    german: "German",
+    italian: "Italian",
+    chooseLanguage: "Choose Language",
   },
-  errorPage: {
-    message: 'Sorry, nothing here... 404',
-    back: 'Go back'
-  }
-}
+  errorPage: { message: "Sorry, nothing here... 404", back: "Go back" },
+};
