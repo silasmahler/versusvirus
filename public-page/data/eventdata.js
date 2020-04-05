e15b5699eda4892c68956de019678be34b0e3337
Johannes Teklote <johannes.teklote@web.de>
2020-04-05 17:17:31 +0200

    init page

---
+++ public-page/data/eventdata.js
@@ -0,0 +1,50 @@
+export default [
+  {
+    "id": 1,
+    "title": "VersusVirus Launch Dinner",
+    "start": "2020-04-03T19:00:00",
+    "end": "2020-04-03T21:00:00",
+  },
+  {
+    "id": 2,
+    "title": "VersusVirus Dinner Day 2",
+    "start": "2020-04-04T19:00:00",
+    "end": "2020-04-04T20:00:00",
+  },
+  {
+    "id": 3,
+    "title": "Sunday Family Dinner",
+    "start": "2020-04-05T11:30:00",
+    "end": "2020-04-05T13:30:00",
+  },
+  {
+    "id": 4,
+    "title": "Cooking Lesson #12",
+    "start": "2020-04-06T18:00:00",
+    "end": "2020-04-06T19:30:00",
+  },
+  {
+    "id": 5,
+    "title": "15 Minute Meals",
+    "start": "2020-04-07T19:00:00",
+    "end": "2020-04-07T19:30:00",
+  },
+  {
+    "id": 6,
+    "title": "Welcome Weekend Pizza",
+    "start": "2020-04-10T19:00:00",
+    "end": "2020-04-10T20:00:00",
+  },
+  {
+    "id": 7,
+    "title": "Cook Together #02",
+    "start": "2020-04-11T18:00:00",
+    "end": "2020-04-11T20:00:00",
+  },
+  {
+    "id": 8,
+    "title": "Sunday Family Dinner",
+    "start": "2020-04-12T11:30:00",
+    "end": "2020-04-12T13:30:00",
+  }
+]
